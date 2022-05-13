68470af5735271afa5dea0c014b79bbc88416557
Mat Ruane <mat@ruane.net.nz>
2022-05-13 18:12:25 +1200

    :white_check_mark: Update tests for styled-components

--- src/tests/CarouselSlide.test.js
+++ src/tests/CarouselSlide.test.js
@@ -1,6 +1,20 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import CarouselSlide from '../CarouselSlide'
+
+describe('Img', () => {
+  let mounted
+  const imgUrl = 'https://example.com/default.jpg'
+
+  beforeEach(() => {
+    const Img = CarouselSlide.defaultProps.Img
+    mounted = mount(<Img src={imgUrl} imgHeight={500} />)
+  })
+
+  it('renders an <img> with the given src', () => {
+    expect(mounted.containsMatchingElement(<img src={imgUrl} />)).toBe(true)
+  })
+})
 
 describe('CarouselSlide', () => {
   let wrapper
@@ -17,14 +31,14 @@ describe('CarouselSlide', () => {
   it('renders a <figure>', () => {
     expect(wrapper.type()).toBe('figure')
   })
-  it('renders and <img> and a <figcaption> as children', () => {
-    expect(wrapper.childAt(0).type()).toBe('img')
+  it('renders and props.Img and a <figcaption> as children', () => {
+    expect(wrapper.childAt(0).type()).toBe(CarouselSlide.defaultProps.Img)
     expect(wrapper.childAt(1).type()).toBe('figcaption')
   })
-  it('passes `imgUrl` through to the <img>', () => {
+  it('passes `imgUrl` through to props.Img', () => {
     const imgUrl = 'https://example.com/image.png'
     wrapper.setProps({ imgUrl })
-    const img = wrapper.find('img')
+    const img = wrapper.find(CarouselSlide.defaultProps.Img)
     expect(img.prop('src')).toBe(imgUrl)
   })
   it('uses `description` and `attribution` as the <figcaption>', () => {
